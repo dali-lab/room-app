cb9e707004229319a4885a036ce3fda7c0807fa3
chelseajoe <chelseaj2021@gmail.com>
2022-04-03 19:25:04 -0400

    Added inactive and active icons to nav-bar

--- navigation/BottomTabs.js
+++ navigation/BottomTabs.js
@@ -1,4 +1,6 @@
+/* eslint-disable global-require */
 import React from 'react';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import CalendarScreen from '../screens/CalendarScreen';
@@ -7,15 +9,50 @@ import { colors } from '../styles/GlobalStyles';
 
 const Tab = createBottomTabNavigator();
 
+const tabBarIcon = (route, focused, color, size) => {
+  let iconSource;
+  if (route.name === 'Home') {
+    iconSource = focused
+      ? require('../assets/active-home.png')
+      : require('../assets/inactive-home.png');
+  } else if (route.name === 'Calendar') {
+    iconSource = focused
+      ? require('../assets/active-calendar.png')
+      : require('../assets/inactive-calendar.png');
+  } else if (route.name === 'Request') {
+    iconSource = focused
+      ? require('../assets/active-request.png')
+      : require('../assets/inactive-request.png');
+  }
+  console.log(iconSource);
+  return <Image source={iconSource} />;
+};
+  // console.log(route);
+  // return null;
+  // // if else based on route, return image
+
 const BottomTabs = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      tabBarOptions={{
-        tabBarActiveTintColor: colors.primarySageGreen,
-        tabBarInactiveTintColor: 'black',
-        labelStyle: { fontSize: 16, marginBottom: 5 },
-      }}
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: colors.primarySageGreen,
+        tabBarLabelStyle: { fontSize: 16 },
+        tabBarStyle: { marginTop: 10 },
+        tabBarIcon: ({ focused, color, size }) => tabBarIcon(route, focused, color, size),
+        // tabBarIcon: ({ focused, color, size }) => {
+        //   let iconSource;
+
+        //   if (route.name === 'Home') {
+        //     iconSource = focused
+        //       ? require('../assets/active-calendar.svg')
+        //       : require('../assets/active-calendar.svg');
+        //   } else iconSource = 'foo';
+
+        //   return <Image source={iconSource} />;
+        // },
+      })}
     >
       <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
