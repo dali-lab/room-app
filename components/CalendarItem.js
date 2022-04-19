3d0ad4595269343f2c5a4f31e6a6e61a09bf007b
kksamps2 <kksamps2@gmail.com>
2022-04-19 16:17:05 -0400

    added swipe

--- components/CalendarItem.js
+++ components/CalendarItem.js
@@ -2,7 +2,44 @@ import React from 'react';
 import {
   StyleSheet, View, Text,
 } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { fonts, dimensions, colors } from '../constants/GlobalStyles';
+
+const LeftActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: 'clamp',
+  });
+  return (
+    <>
+      <View style={styles.swipeItem}>
+        <Text
+          style={{
+            color: 'white',
+            paddingHorizontal: 10,
+            fontWeight: '600',
+            transform: [{ scale }],
+          }}
+        >
+          Edit
+        </Text>
+      </View>
+      <View style={styles.swipeItem}>
+        <Text
+          style={{
+            color: 'white',
+            paddingHorizontal: 5,
+            fontWeight: '600',
+            transform: [{ scale }],
+          }}
+        >
+          Delete
+        </Text>
+      </View>
+    </>
+  );
+};
 
 const CalendarItem = (props) => {
   const {
@@ -10,14 +47,16 @@ const CalendarItem = (props) => {
   } = props;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.icon}>{`${author} icon`}</Text>
-      <View style={styles.description}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.text}>{`${start} - ${end}`}</Text>
+    <Swipeable renderLeftActions={LeftActions}>
+      <View style={styles.container}>
+        <Text style={styles.icon}>{`${author} icon`}</Text>
+        <View style={styles.description}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.text}>{`${start} - ${end}`}</Text>
+        </View>
+        <Text style={styles.text}># of Likes</Text>
       </View>
-      <Text style={styles.text}># of Likes</Text>
-    </View>
+    </Swipeable>
   );
 };
 
@@ -27,7 +66,18 @@ const styles = StyleSheet.create({
     height: 80,
     backgroundColor: colors.backgroundSageGreen,
     margin: 20,
-    padding: 10,
+    padding: 5,
+    borderColor: colors.darkSageGreen,
+    borderLeftWidth: 5,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  description: {
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    height: 60,
+    width: 200,
   },
   title: {
     fontSize: fonts.largeText,
@@ -38,6 +88,16 @@ const styles = StyleSheet.create({
     fontSize: fonts.smallText,
     color: colors.darkSageGreen,
     marginLeft: 20,
+  },
+  icon: {
+    fontSize: 10,
+    fontWeight: '300',
+    marginLeft: 20,
+    width: 35,
+  },
+  swipeItem: {
+    backgroundColor: colors.darkSageGreen,
+    justifyContent: 'center',
   },
 });
 
--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import {
-  StyleSheet, ScrollView, SafeAreaView,
+  StyleSheet, ScrollView, SafeAreaView, Text,
 } from 'react-native';
 import { connect } from 'react-redux';
 import CalendarItem from '../components/CalendarItem';
 import { getAllCalendarEvents } from '../store/actions';
-import { fonts } from '../constants/GlobalStyles';
+import { fonts, colors } from '../constants/GlobalStyles';
 
 const CalendarScreen = (props) => {
   const { getCalendarEvents, calendarEvents } = props;
@@ -19,6 +19,8 @@ const CalendarScreen = (props) => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Upcoming Events</Text>
+      <Text style={styles.subtitle}>Swipe left on an event to edit or delete it</Text>
       <ScrollView>
         {calendarEvents?.map(({
           id, title, start, end, author,
@@ -32,6 +34,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: fonts.large24,
+    textAlign: 'left',
+    color: colors.darkSageGreen,
+    marginLeft: 20,
+    marginTop: 20,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: fonts.smallText,
+    textAlign: 'left',
+    color: colors.darkSageGreen,
+    marginLeft: 20,
+    marginBottom: -15,
   },
   text: {
     fontSize: fonts.largeText,
