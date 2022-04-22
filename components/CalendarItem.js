cd3b4e4125db0e62e70b78039ea108cfda33008c
kksamps2 <kksamps2@gmail.com>
2022-04-22 12:09:00 -0400

    added modal, still working on functionality

--- components/CalendarItem.js
+++ components/CalendarItem.js
@@ -1,35 +1,57 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, View, Text, TouchableOpacity,
+  StyleSheet, View, Text, TouchableOpacity, Modal,
 } from 'react-native';
 import moment from 'moment';
 import { fonts, dimensions, colors } from '../constants/GlobalStyles';
 
 const CalendarItem = (props) => {
+  const [showModal, setShowModal] = useState(false);
   const {
     title, start, end, author,
   } = props;
-
   return (
-    <View style={styles.container}>
-      <View style={styles.eventContainer}>
-        <Text style={styles.icon}>{`${author} icon`}</Text>
-        <View style={styles.description}>
-          <Text style={styles.title}>{title}</Text>
-          <Text style={styles.text}>{`${moment(start).format('h:mm a')} - ${moment(end).format('h:mm a')}`}</Text>
-        </View>
-        <Text style={styles.text}># of Likes</Text>
-      </View>
-      <View style={styles.approveContainer}>
-        <TouchableOpacity style={styles.approveButton} onPress={() => console.log('approve')}>
-          <View>
-            <Text>Approve</Text>
+    <View>
+      <View style={styles.container}>
+        <View style={styles.eventContainer}>
+          <Text style={styles.icon}>{`${author} icon`}</Text>
+          <View style={styles.description}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.text}>{`${moment(start).format('h:mm a')} - ${moment(end).format('h:mm a')}`}</Text>
           </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.letsTalkButton} onPress={() => console.log('lets talk')}>
-          <Text>Let's Talk</Text>
-        </TouchableOpacity>
+          <Text style={styles.text}># of Likes</Text>
+        </View>
+        <View style={styles.approveContainer}>
+          <TouchableOpacity style={styles.approveButton} onPress={() => console.log('approved!')}>
+            <View>
+              <Text>Approve</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.letsTalkButton} onPress={() => setShowModal(!showModal)}>
+            <Text>Let's Talk</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <Modal
+          animationType="slide"
+          visible={showModal}
+          onRequestClose={() => {
+            console.log('Modal has been closed.');
+          }}
+        >
+          {/* All views of Modal */}
+          {/* Animation can be slide, slide, none */}
+          <View style={styles.modalContainer}>
+            <Text style={styles.text}>Modal is open!</Text>
+            <TouchableOpacity style={styles.letsTalkButton} onPress={() => setShowModal(!showModal)}>
+              <View>
+                <Text>Close Modal</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </Modal>
       </View>
     </View>
   );
@@ -64,6 +86,18 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     borderColor: colors.darkSageGreen,
     borderLeftWidth: 5,
+  },
+  modalContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderStyle: 'solid',
+    width: dimensions.screenWidth * 0.9,
+    height: 80,
+    marginTop: 100,
+    margin: 20,
+    alignItems: 'center',
   },
   description: {
     flexDirection: 'column',
