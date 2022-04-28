77b8ffac20156762afacdf180d8f80c3b804dd8c
Kaylie Sampson <kksamps2@gmail.com>
2022-04-28 15:11:49 -0400

    styling for modal

--- components/CalendarItem.js
+++ components/CalendarItem.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import {
-  StyleSheet, View, Text, TouchableOpacity, Modal, TextInput,
+  StyleSheet, View, Text, TouchableOpacity, Modal, TextInput, Switch,
 } from 'react-native';
 import moment from 'moment';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
@@ -10,8 +10,11 @@ import { fonts, dimensions, colors } from '../constants/GlobalStyles';
 import { updateCalendarEvent } from '../store/actions/calendarEvent';
 
 const LeftActions = () => {
+  // const [showEditModal, setshowEditModal] = false;
+  // const setShowModal = () => { !showEditModal; };
   return (
     <View style={styles.swipeContainer}>
+      {/* <TouchableOpacity style={styles.swipeItem} onPress={() => setshowEditModal(!showEditModal)}> */}
       <TouchableOpacity style={styles.swipeItem} onPress={() => console.log('pressed edit button')}>
         <Text style={styles.swipeItemText}>Edit</Text>
       </TouchableOpacity>
@@ -23,7 +26,7 @@ const LeftActions = () => {
 };
 
 const CalendarItem = (props) => {
-  const [showModal, setShowModal] = useState(false);
+  const [showLetsTalkModal, setshowLetsTalkModal] = useState(false);
   const {
     id, title, start, end, author, user, updateEvent, approvals, users,
   } = props;
@@ -50,7 +53,7 @@ const CalendarItem = (props) => {
           </View>
         )
         : (
-          <View style={styles.noswipey}>
+          <View>
             <View style={styles.eventContainer}>
               <Text style={styles.icon}>{`${author} icon`}</Text>
               <View style={styles.description}>
@@ -65,20 +68,87 @@ const CalendarItem = (props) => {
                   <Text>Approve</Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.letsTalkButton} onPress={() => setShowModal(!showModal)}>
+              <TouchableOpacity style={styles.letsTalkButton} onPress={() => setshowLetsTalkModal(!showLetsTalkModal)}>
                 <Text>Let's Talk</Text>
               </TouchableOpacity>
             </View>
             <View>
               <Modal
                 animationType="fade"
-                visible={showModal}
+                visible={showLetsTalkModal}
                 transparent
                 onRequestClose={() => {
                   console.log('Modal has been closed.');
                 }}
               >
-                <View style={styles.modalContainer}>
+                <View style={styles.swipeModalContainer}>
+                  <TouchableOpacity style={styles.exitButton} onPress={() => setshowLetsTalkModal(!showLetsTalkModal)}>
+                    <Text style={styles.text}>X</Text>
+                  </TouchableOpacity>
+
+                  <Text style={styles.icon}>INSERT CALENDAR LOGO INSERT CALENDAR LOGO INSERT CALENDAR LOGO</Text>
+                  <View style={styles.description}>
+                    <Text style={styles.title}>EDIT EVENT</Text>
+                  </View>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.text}>Title</Text>
+                    <View style={styles.inputContainer}>
+                      <TextInput
+                        style={styles.inputTitle}
+                        defaultValue={`${title}`}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.text}>All-Day</Text>
+                    <View style={styles.inputContainer}>
+                      <Switch />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.text}>Start</Text>
+                    <View style={styles.inputContainer}>
+                      <TextInput
+                        style={styles.inputTime}
+                        defaultValue={`${moment(start).format('MMM DD')}`}
+                      />
+                      <TextInput
+                        style={styles.inputTime}
+                        defaultValue={`${moment(start).format('h:mm a')}`}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Text style={styles.text}>End</Text>
+                    <View style={styles.inputContainer}>
+                      <TextInput
+                        style={styles.inputTime}
+                        defaultValue={`${moment(end).format('MMM DD')}`}
+                      />
+                      <TextInput
+                        style={styles.inputTime}
+                        defaultValue={`${moment(end).format('h:mm a')}`}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity style={styles.modalButton} onPress={() => setshowLetsTalkModal(!showLetsTalkModal)}>
+                      <Text style={{ color: '#FFFFFF' }}>Save</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </Modal>
+            </View>
+            {/* <View>
+              <Modal
+                animationType="fade"
+                visible={showLetsTalkModal}
+                transparent
+                onRequestClose={() => {
+                  console.log('Modal has been closed.');
+                }}
+              >
+                <View style={styles.letsTalkModalContainer}>
                   <View style={styles.description}>
                     <Text style={styles.title}>{title}</Text>
                     <Text style={styles.text}>{`${moment(start).format('h:mm a')} - ${moment(end).format('h:mm a')}`}</Text>
@@ -86,21 +156,21 @@ const CalendarItem = (props) => {
                   <View style={{ flexDirection: 'row' }}>
                     <Text style={styles.text}>Message</Text>
                     <TextInput
-                      style={styles.input}
+                      style={styles.inputTitle}
                       placeholder="Optional"
                     />
                   </View>
                   <View style={{ flexDirection: 'row' }}>
-                    <TouchableOpacity style={styles.modalButton} onPress={() => setShowModal(!showModal)}>
+                    <TouchableOpacity style={styles.modalButton} onPress={() => setshowLetsTalkModal(!showLetsTalkModal)}>
                       <Text style={{ color: '#FFFFFF' }}>Cancel</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.modalButton} onPress={() => setShowModal(!showModal)}>
+                    <TouchableOpacity style={styles.modalButton} onPress={() => setshowLetsTalkModal(!showLetsTalkModal)}>
                       <Text style={{ color: '#FFFFFF' }}>Send</Text>
                     </TouchableOpacity>
                   </View>
                 </View>
               </Modal>
-            </View>
+            </View> */}
           </View>
         )}
     </View>
@@ -137,11 +207,27 @@ const styles = StyleSheet.create({
     borderColor: colors.darkSageGreen,
     borderLeftWidth: 5,
   },
-  modalContainer: {
+  letsTalkModalContainer: {
     backgroundColor: colors.backgroundSageGreen,
     width: dimensions.screenWidth * 0.8,
     height: 140,
     marginTop: dimensions.screenHeight * 0.3,
+    margin: dimensions.screenWidth * 0.1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowRadius: 60,
+    shadowOpacity: 0.2,
+  },
+  swipeModalContainer: {
+    backgroundColor: colors.backgroundSageGreen,
+    width: dimensions.screenWidth * 0.8,
+    height: dimensions.screenHeight * 0.5,
+    marginTop: dimensions.screenHeight * 0.2,
     margin: dimensions.screenWidth * 0.1,
     alignItems: 'center',
     justifyContent: 'space-around',
@@ -185,15 +271,24 @@ const styles = StyleSheet.create({
     width: dimensions.screenWidth * 0.3,
     alignItems: 'center',
   },
-  input: {
+  inputTitle: {
     backgroundColor: '#FFFFFF',
     width: dimensions.screenWidth * 0.4,
     marginLeft: 10,
+    height: dimensions.screenHeight * 0.03,
+  },
+  inputTime: {
+    backgroundColor: '#FFFFFF',
+    width: dimensions.screenWidth * 0.2,
+    marginLeft: 10,
+    height: dimensions.screenHeight * 0.03,
   },
   modalButton: {
     backgroundColor: colors.darkSageGreen,
     width: dimensions.screenWidth * 0.3,
     alignItems: 'center',
+    justifyContent: 'center',
+    height: dimensions.screenHeight * 0.03,
     margin: 10,
   },
   swipeItem: {
@@ -205,6 +300,22 @@ const styles = StyleSheet.create({
   swipeItemText: {
     fontSize: fonts.smallText,
     color: '#ffffff',
+  },
+  inputContainer: {
+    width: dimensions.screenWidth * 0.5,
+    flexDirection: 'row',
+  },
+  exitButton: {
+    borderWidth: 3,
+    borderColor: colors.darkSageGreen,
+    borderRadius: 20,
+
+    width: dimensions.screenWidth * 0.1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: dimensions.screenHeight * 0.04,
+    marginRight: dimensions.screenWidth * 0.6,
+    marginTop: 10,
   },
 });
 
