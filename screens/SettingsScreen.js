382eff16d19455bc8e1448ab597da0f186708ba4
Kaylie Sampson <kksamps2@gmail.com>
2022-05-05 22:08:53 -0400

    leave room functionality

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet, SafeAreaView, Text, View, TextInput, TouchableOpacity,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { fonts, colors, dimensions } from '../constants/GlobalStyles';
-import { updateUser } from '../store/actions';
+import { updateUser, getAllUsers } from '../store/actions';
 import UserIcon from '../components/UserIcon';
+// import { getUser } from '../services/user';
 
 const SettingsScreen = (props) => {
   const {
-    user, users, roomCode, updateNewUser,
+    user, roomCode, updateNewUser, getUsers,
   } = props;
+
+  useEffect(() => {
+    getUsers(user?.roomCode);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
   const [newFirstName, setnewFirstName] = useState(user.firstName);
   const [newLastName, setnewLastName] = useState(user.lastName);
   const [newIconColor, setnewIconColor] = useState(user.iconColor);
@@ -29,6 +35,56 @@ const SettingsScreen = (props) => {
       roommates: user.roommates,
     };
     updateNewUser(user.id, newUser, roomCode);
+  };
+  const handleLeaveRoom = () => {
+    const newUser = {
+      email: user.email,
+      password: user.password,
+      firstName: newFirstName,
+      lastName: newLastName,
+      isHome: user.isHome,
+      roomCode: '',
+      guestType: user.guestType,
+      iconColor: newIconColor,
+      numGuests: user.numGuests,
+      roommates: [],
+    };
+    updateNewUser(user.id, newUser, roomCode);
+    user.roommates.forEach((userCurr) => {
+      const newUser = {
+        email: userCurr.email,
+        password: userCurr.password,
+        firstName: userCurr.firstName,
+        lastName: userCurr.lastName,
+        isHome: userCurr.isHome,
+        roomCode: userCurr.roomCode,
+        guestType: userCurr.guestType,
+        iconColor: userCurr.iconColor,
+        numGuests: userCurr.numGuests,
+        roommates: userCurr.roommates.filter((roommate) => (roommate !== user)),
+      };
+      updateNewUser(userCurr.id, newUser, roomCode);
+    });
+  };
+  const handleDeleteRoom = () => {
+    // dont allow if user still has roommates or others have same roomcode
+    if (user.roommates.length === 0) {
+      const newUser = {
+        email: user.email,
+        password: user.password,
+        firstName: newFirstName,
+        lastName: newLastName,
+        isHome: user.isHome,
+        roomCode: '',
+        guestType: user.guestType,
+        iconColor: newIconColor,
+        numGuests: user.numGuests,
+        roommates: user.roommates,
+      };
+      updateNewUser(user.id, newUser, roomCode);
+    } else {
+      console.log('action not allowed since other roommates use this code');
+    }
   };
   return (
     <SafeAreaView>
@@ -54,19 +110,43 @@ const SettingsScreen = (props) => {
       <View style={styles.subContainer}>
         <Text style={styles.text}>Icon Color</Text>
         <View style={styles.colorPicker}>
-          <TouchableOpacity style={styles.buttonDarkPurple} onPress={() => setnewIconColor(colors.indigo700)} />
-          <TouchableOpacity style={styles.buttonYellow} onPress={() => setnewIconColor('yellow')} />
-          <TouchableOpacity style={styles.buttonLightPurple} onPress={() => setnewIconColor(colors.indigo300)} />
+          {user.iconColor === colors.indigo700
+            ? (
+              <TouchableOpacity style={styles.buttonDarkPurple} onPress={() => setnewIconColor(colors.indigo700)}>
+                <Text style={{ textAlign: 'center', fontSize: 30 }}>X</Text>
+              </TouchableOpacity>
+            )
+            : (
+              <TouchableOpacity style={styles.buttonDarkPurple} onPress={() => setnewIconColor(colors.indigo700)} />
+            )}
+          {user.iconColor === '#FFFF00'
+            ? (
+              <TouchableOpacity style={styles.buttonYellow} onPress={() => setnewIconColor('#FFFF00')}>
+                <Text style={{ textAlign: 'center', fontSize: 30 }}>X</Text>
+              </TouchableOpacity>
+            )
+            : (
+              <TouchableOpacity style={styles.buttonYellow} onPress={() => setnewIconColor('#FFFF00')} />
+            )}
+          {user.iconColor === colors.indigo300
+            ? (
+              <TouchableOpacity style={styles.buttonLightPurple} onPress={() => setnewIconColor(colors.indigo300)}>
+                <Text style={{ textAlign: 'center', fontSize: 30 }}>X</Text>
+              </TouchableOpacity>
+            )
+            : (
+              <TouchableOpacity style={styles.buttonLightPurple} onPress={() => setnewIconColor(colors.indigo300)} />
+            )}
         </View>
 
       </View>
       <View style={styles.subContainer}>
-        <TouchableOpacity style={styles.modalButton}>
+        <TouchableOpacity style={styles.modalButton} onPress={handleLeaveRoom}>
           <Text style={{ color: '#FFFFFF', fontSize: fonts.largeText }}>Leave Room</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.subContainer}>
-        <TouchableOpacity style={styles.modalButton}>
+        <TouchableOpacity style={styles.modalButton} onPress={handleDeleteRoom}>
           <Text style={{ color: '#FFFFFF', fontSize: fonts.largeText }}>Delete Room</Text>
         </TouchableOpacity>
       </View>
@@ -118,6 +198,7 @@ const styles = StyleSheet.create({
     width: 30,
     marginHorizontal: 10,
     backgroundColor: colors.indigo700,
+    justifyContent: 'center',
   },
   buttonLightPurple: {
     height: 30,
@@ -129,14 +210,14 @@ const styles = StyleSheet.create({
     height: 30,
     width: 30,
     marginHorizontal: 10,
-    backgroundColor: 'yellow',
+    backgroundColor: '#FFFF00',
+    justifyContent: 'center',
   },
 });
 
 const mapStateToProps = (state) => {
   return {
-    users: state.user.allUsers,
-    user: state.user.user,
+    user: state.user.user, 
     roomCode: state.user.roomCode,
   };
 };
@@ -144,6 +225,12 @@ const mapDispatchToProps = (dispatch) => {
   return {
     updateNewUser: (id, user, roomcode) => {
       dispatch(updateUser(id, user, roomcode));
+    },
+    getUsers: (roomcode) => {
+      dispatch(getAllUsers(roomcode));
+    },
+    getUser: (id) => {
+      dispatch(getAllUsers(id));
     },
   };
 };
